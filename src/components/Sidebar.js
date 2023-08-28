0f01750c9377ebbe2d4d205b397ca56f29936d12
badtype <g.c.faina@gmail.com>
2023-08-28 10:26:04 +0200

    sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,22 @@ import React from "react";
 import { LinkComponent, SocialComponent } from "../utils/links";
 import { VscArrowSmallLeft } from "react-icons/vsc";
 const Sidebar = () => {
-  return <aside>SideBar</aside>;
+  return <aside className="sidebar show-sidebar">
+    <div className="sidebar-content">
+      <header className="nav-header container">
+        <div className="nav-brand">
+          <h3>Drink Team</h3>
+        </div>
+        <button className="nav-toggler btn icon-btn">
+          <VscArrowSmallLeft cloassName="nav-icon" />
+        </button>
+      </header>
+      <div className="container">
+        <LinkComponent classLink="sidebar-links" />
+      </div>
+      <SocialComponent classSocial="sidebar-social" />
+    </div>
+  </aside>;
 };
 
 export default Sidebar;
