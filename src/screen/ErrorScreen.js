08133490b723161f2ba697f8519b7cfb275fe705
badtype <g.c.faina@gmail.com>
2023-08-28 16:46:37 +0200

    error page

--- src/screen/ErrorScreen.js
+++ src/screen/ErrorScreen.js
@@ -4,15 +4,43 @@ import Lottie from "react-lottie";
 import animationData from "../assets/animation/not-found.json";
 import { Link } from "react-router-dom";
 const ErrorScreen = () => {
-  return <Wrapper>Error Screen</Wrapper>;
+  return <Wrapper>
+    <h3>
+      Page not found
+    </h3>
+
+    <Lottie options={
+      {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      }
+    }
+      width={500}
+      height={500}
+    />
+
+    <Link to="/" className="btn btn-primary">
+      Back to Home
+    </Link>
+  </Wrapper>;
 };
 
 const Wrapper = styled.section`
   min-height: 86.2vh;
   display: grid;
+  display: -ms-grid;
+  display: -moz-grid;
+  display: -webkit-grid;
   place-items: center;
   h3 {
     text-transform: uppercase;
+  }
+  .btn{
+    border-radius: 22px;
   }
   @media screen and (min-width: 992px) {
     min-height: 84.5vh;
