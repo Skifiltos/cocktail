886f37b1e020709d9835d27473840802fea2c215
badtype <g.c.faina@gmail.com>
2023-09-17 20:16:07 +0200

    Contact Screen

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -4,7 +4,95 @@ import backImage from "../assets/image/Contact-hero.png";
 
 
 const ContactScreen = () => {
-  return <>ContactScreen</>;
+  return <>
+    <Hero image={backImage} disableOverlay />
+    <div className="contact-hero container">
+      <div contact-hero-text>
+        <div className="contact-hero-title">
+          <h2 className="contact-title">
+            Vorresti aggiungere un Cocktail?
+          </h2>
+          <h4 className="contact-subtitle">
+            Il nostro team è sempre disponibile per valutare possibili
+            nuove ricette ed aggiungerle a quelle del nostro database.
+          </h4>
+        </div>
+      </div>
+      <div className="contact-form-container container">
+
+        <form className="contact-form container">
+
+          <div className="form-group">
+            <label htmlFor="nome">
+              Nome
+            </label>
+            <input
+              type="text"
+              id="nome"
+              className="input"
+              placeholder="Inserisci il tuo nome"
+            />
+            <hr />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="cognome">
+              Cognome
+            </label>
+            <input
+              type="text"
+              id="cognome"
+              className="input"
+              placeholder="Inserisci il tuo cognome"
+            />
+            <hr />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="input"
+              placeholder="Inserisci la tua email"
+            />
+            <hr />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="telefono">
+              Telefono
+            </label>
+            <input
+              type="text"
+              id="telefono"
+              className="input"
+              placeholder="Inserisci il tuo telefono"
+            />
+            <hr />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="ricetta">
+              La tua ricetta
+            </label>
+            <input
+              type="text"
+              id="ricetta"
+              className="input"
+              placeholder="Describi la tua ricetta"
+            />
+            <hr />
+          </div>
+
+          <button className="btn btn-primary">Invia la tua ricetta</button>
+
+        </form>
+      </div >
+    </div >
+  </>;
 };
 
 export default ContactScreen;
