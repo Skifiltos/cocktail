da18ddbcf73639538a61876697d6b03c590129af
badtype <g.c.faina@gmail.com>
2023-09-18 14:34:39 +0200

    Home screen input

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -7,44 +7,72 @@ import { Link } from "react-router-dom";
 
 
 const HomeScreen = () => {
-  return <>
-    <Hero>
-      <div className="home-hero">
-        <div className="home-hero-text">
-          <div className="home-hero-title">
-            <h2 className="brand-color">
-              WIKI DRINK
-            </h2>
-            <h4>Tutti i cocktail del mondo a portata di click!</h4>
+  const [input, setInput] = useState("negroni");
+  return (
+    <>
+      <Hero>
+        <div className="home-hero">
+          <div className="home-hero-text">
+            <div className="home-hero-title">
+              <h2 className="brand-color">
+                WIKI DRINK
+              </h2>
+              <h4>Tutti i cocktail del mondo a portata di click!</h4>
+            </div>
+            <p>
+              Wiki Drink è un database internazionale che mette a tua
+              disposizione, in maniera <span className="brand-color">Gratuita</span>, le ricette dei più importanti e
+              diffusi cocktail al mondo.
+            </p>
+            <Link to="/about" className="btn btn-primary">
+              Scopri di più
+            </Link>
           </div>
-          <p>
-            Wiki Drink è un database internazionale che mette a tua
-            disposizione, in maniera <span className="brand-color">Gratuita</span>, le ricette dei più importanti e
-            diffusi cocktail al mondo.
-          </p>
-          <Link to="/about" className="btn btn-primary">
-            Scopri di più
-          </Link>
+          <div className="home-hero-img">
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
+                reverse: true,
+                animationData: animationData,
+                rendererSettings: {
+                  preserveAspectRatio: "xMidYMid slice"
+                }
+              }}
+              height={350}
+            // width={400}
+            />
+          </div>
         </div>
-        <div className="home-hero-img">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              reverse: true,
-              animationData: animationData,
-              rendererSettings: {
-                preserveAspectRatio: "xMidYMid slice"
-              }
-            }}
-            height={350}
-          // width={400}
-          />
+      </Hero>
+      <section className="container home-screen">
+        <div className="search-bar">
+          <div className="form-container">
+            <form >
+              <label htmlFor="drink">
+                <h4>
+                  Cerca il tuo Drink
+                </h4>
+              </label>
+              <div className="input-search">
+                <input
+                  id="drink"
+                  className="input"
+                  placeholder={input}
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+                <button className="btn icon-btn" type="submit">
+                  <FaSearch className="icon" />
+                </button>
+              </div>
+            </form>
+          </div>
+          <p className="result">3 risultati</p>
         </div>
-      </div>
-    </Hero>
-
-  </>;
+      </section >
+    </>
+  );
 };
 
 export default HomeScreen;
